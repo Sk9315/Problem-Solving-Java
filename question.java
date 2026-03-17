943759ec1fcb0d96c22d48d771ccf09ca472c7f1
sacchidanand Pandey <saurabhpnd9315@gmail.com>
2026-03-18 03:01:21 +0530

    initial commit

--- question.java
+++ question.java
@@ -1,14 +1,4 @@
-/*
-class Node{
-    int data;
-    Node next;
 
-    Node(int x){
-        data = x;
-        next = null;
-    }
-}
-*/
 class Solution {
     public Node insertAtEnd(Node head, int x) {
        
