b1a3844e12e2a44b0c4fbd99d6964d71dfd9bfc6
sacchidanand Pandey <saurabhpnd9315@gmail.com>
2026-03-18 01:46:12 +0530

    initial commit

---
+++ question.java
@@ -0,0 +1,34 @@
+/*
+class Node{
+    int data;
+    Node next;
+
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+*/
+class Solution {
+    public Node insertAtEnd(Node head, int x) {
+       
+        //handling the edge cases
+        if(head==null){
+            head=new Node(x);
+            return head;
+            
+        }
+
+        //writing the main logic
+        Node temp=head;
+        while(temp.next!=null){
+            temp=temp.next;
+        }
+        Node val=new Node(x);
+        temp.next=val;
+        val.next=null;
+    
+        //returning the value
+        return head;
+    }
+}
